83fe3a67c0e37db9ee5e417a98936f45a9b53d8a
Haruki <aqunkh1@gmail.com>
2024-05-24 22:37:24 +0900

    Deploy static site to GitHub Pages

---
+++ _next/static/EDWQJ991kjfBhnOtuqf6l/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/165-128050b3f662dbfe.js","static/chunks/pages/index-33c313e052e1f94d.js"],"/_error":["static/chunks/pages/_error-77823ddac6993d35.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
