b3620e57c7ef6723c278eefaa258b730e3331514
Haruki <aqunkh1@gmail.com>
2024-05-28 21:08:12 +0900

    s

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@next/next/recommended",
+  ],
+  rules: { "react/react-in-jsx-scope": "off" },
+  plugins: ["@typescript-eslint", "react"],
+};
