88bb941d9cb58de41b695856f2324aa6abd2018b
Haruki <aqunkh1@gmail.com>
2024-05-28 19:33:19 +0900

    Updates

--- _next/static/zo2H-xNEyrHRnhBfnfmDz/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/165-128050b3f662dbfe.js","static/chunks/pages/index-82db188f6cd8edae.js"],"/_error":["static/chunks/pages/_error-77823ddac6993d35.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
