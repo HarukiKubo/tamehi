ee035cd359bf89b1b4373d213b0a00c9e4078149
Haruki <aqunkh1@gmail.com>
2024-05-28 19:46:08 +0900

    ｄ

--- next.config.mjs
+++ next.config.mjs
@@ -2,8 +2,8 @@
 const nextConfig = {
   reactStrictMode: true,
   output: "export",
-  basePath: "/tamehi", // リポジトリ名に合わせて設定
-  assetPrefix: "/tamehi", // 静的ファイルのプレフィックスを設定
+  basePath: "/tamehi",
+  assetPrefix: "/tamehi",
   env: {
     NEXT_PUBLIC_RESAS_API_KEY: process.env.NEXT_PUBLIC_RESAS_API_KEY,
     NEXT_PUBLIC_PREFECTURES_API_URL:
@@ -11,10 +11,14 @@ const nextConfig = {
     NEXT_PUBLIC_POPULATION_API_URL: process.env.NEXT_PUBLIC_POPULATION_API_URL,
   },
   exportPathMap: async function () {
+    console.log("Export Path Map called with basePath:", "/tamehi");
     return {
       "/": { page: "/" },
     };
   },
 };
+
+console.log("Next config loaded with basePath:", nextConfig.basePath);
+console.log("Next config loaded with assetPrefix:", nextConfig.assetPrefix);
 
 export default nextConfig;
