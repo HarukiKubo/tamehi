63e39676aaf4e4c546a49da697aedbecf80a30f2
Haruki <aqunkh1@gmail.com>
2024-05-28 20:22:06 +0900

    noj

--- next.config.mjs
+++ next.config.mjs
@@ -1,24 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: "export",
-  basePath: "/tamehi",
-  assetPrefix: "/tamehi",
-  env: {
-    NEXT_PUBLIC_RESAS_API_KEY: process.env.NEXT_PUBLIC_RESAS_API_KEY,
-    NEXT_PUBLIC_PREFECTURES_API_URL:
-      process.env.NEXT_PUBLIC_PREFECTURES_API_URL,
-    NEXT_PUBLIC_POPULATION_API_URL: process.env.NEXT_PUBLIC_POPULATION_API_URL,
-  },
-  exportPathMap: async function () {
-    console.log("Export Path Map called with basePath:", "/tamehi");
-    return {
-      "/": { page: "/" },
-    };
-  },
 };
-
-console.log("Next config loaded with basePath:", nextConfig.basePath);
-console.log("Next config loaded with assetPrefix:", nextConfig.assetPrefix);
 
 export default nextConfig;
