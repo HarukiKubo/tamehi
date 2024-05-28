590a7908cb061f14028d41817abaaf18b93b9ec5
Haruki <aqunkh1@gmail.com>
2024-05-28 23:32:09 +0900

    c

--- next.config.js
+++ next.config.js
@@ -1,17 +1,7 @@
-<<<<<<< HEAD
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  distDir: "out",
+  // その他の設定があればここに記載します
 };
-=======
-const path = require("path");
->>>>>>> 0508489bd3762ac4b641ad657b20b6d77373a170
 
-module.exports = {
-  reactStrictMode: true,
-  webpack: (config) => {
-    config.resolve.alias["@"] = path.resolve(__dirname, "src");
-    return config;
-  },
-};
+module.exports = nextConfig;
