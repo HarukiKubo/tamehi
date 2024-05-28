65b92824982f66e48f089769488ea67866b1fbe9
Haruki <aqunkh1@gmail.com>
2024-05-28 20:32:07 +0900

    sitayo

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,3 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
+module.exports = {
+  target: "serverless", // または 'experimental-serverless-trace' を使用する場合もあります
 };
-
-export default nextConfig;
